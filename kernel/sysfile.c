94cf4c70c031f94542d3c00fb6476e63c7ac9752
thomas-girard <t.girard912@laposte.net>
2021-11-05 18:20:11 +0100

    activité 4.5

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -498,8 +498,9 @@ sys_create_mutex(void)
   }
 
   f->type = FD_MUTEX;
+  initsleeplock(&le_futex, "le saviez-vous ? le_futex est mon pseudo telegram :) ");
 
-  le_futex.locked = 0;
+  //le_futex.locked = 0;
   f->mutex = le_futex;
   return fd;
 }
@@ -507,12 +508,36 @@ sys_create_mutex(void)
 uint64
 sys_acquire_mutex(void)
 {
-  return 0;
+  struct file *f;
+
+  if(argfd(0, 0, &f) < 0)
+    return -1;
+
+  if (f->type != FD_MUTEX) {
+    return -1;
+  }
+//if (f->mutex.locked == 0) {
+acquiresleep(&f->mutex);
+//}
+return 0;
 }
 
 uint64
 sys_release_mutex(void)
 {
+  struct file *f;
 
-  return 0;
+  if(argfd(0, 0, &f) < 0)
+    return -1;
+
+  if (f->type != FD_MUTEX) {
+    return -1;
+  }
+  if (f->mutex.locked == 1 && f->mutex.pid == myproc()->pid ) {
+    releasesleep(&f->mutex);
+    return 0;
+  }
+
+  return -1;
+
 }
